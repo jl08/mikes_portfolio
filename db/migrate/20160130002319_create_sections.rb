370645b260f20df23c005d31e58b3c30b29873de
Jenny Lee <leejenny0403@gmail.com>
2016-01-30 13:14:55 -0500

    add posts scaffold

---
+++ db/migrate/20160130002319_create_sections.rb
@@ -0,0 +1,8 @@
+class CreateSections < ActiveRecord::Migration
+  def change
+    create_table :sections do |t|
+      t.string :name, null: false
+      t.timestamps null: false
+    end
+  end
+end
