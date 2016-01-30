fa8b633518334bf2fe23ce968c98118310f88f7f
Jenny Lee <leejenny0403@gmail.com>
2016-01-30 15:10:59 -0500

    added paperclip defaults to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWSAccessKeyId'],
+      :secret_access_key => ENV['AWSSecretKey']
+    }
+  }
 end
