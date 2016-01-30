812800c100c23ec81ba84c1c7c8d34ed68298211
Jenny Lee <leejenny0403@gmail.com>
2016-01-30 15:05:06 -0500

    mpv works

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130181207) do
+ActiveRecord::Schema.define(version: 20160130194953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",       null: false
-    t.string   "description", null: false
-    t.string   "section",     null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "title",              null: false
+    t.string   "description",        null: false
+    t.string   "section",            null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
